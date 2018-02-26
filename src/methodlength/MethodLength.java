162e3a4eb8101d9f4e0a7ac0bb674f5fde562b23
Mariana Barreiros <maridepp1@hotmail.com>
2018-02-26 13:01:08 -0300

    Primeiro commit

---
+++ src/methodlength/MethodLength.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package methodlength;
+
+/**
+ *
+ * @author Win-7
+ */
+public class MethodLength {
+    
+    public static void main(String[] Args){
+        String s = new String();
+        char[] charArray = new char[5];
+        String s1 = "Mariana Barreiros";
+        
+        System.out.println(s.length());
+        System.out.println(charArray.length);
+        System.out.println(s1.length());
+    }
+}
