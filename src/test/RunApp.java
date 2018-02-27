f8154366fca9800a412270ada8264d0ad61c00f5
Mariana Barreiros <maridepp1@hotmail.com>
2018-02-26 21:27:49 -0300

    inclusão do método substring

--- src/test/RunApp.java
+++ src/test/RunApp.java
@@ -61,10 +61,14 @@ public class RunApp {
         
 //       PARA OS MÉTODOS INDEXOF E LASTINDEXOF
 //         IndexOf i = new IndexOf();
-         System.out.println(s1.indexOf('a'));
+         /*System.out.println(s1.indexOf('a'));
          System.out.println(s1.indexOf('a', 3));
          System.out.println(s1.lastIndexOf('a'));
-         System.out.println(s1.lastIndexOf('a', 4));
+         System.out.println(s1.lastIndexOf('a', 4));*/
+         
+//      MÉTODO SUBSTRING
+        System.out.println(s1.substring(2));
+        System.out.println(s1.substring(0, 7));
         
     }
 }
