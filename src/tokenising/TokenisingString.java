4f397ae6d09cee8c2cfcbab22c5118ab88dbaf43
Mariana Barreiros <maridepp1@hotmail.com>
2018-03-10 16:14:15 -0300

    leitura de um arquivo

--- src/tokenising/TokenisingString.java
+++ src/tokenising/TokenisingString.java
@@ -5,7 +5,11 @@
  */
 package tokenising;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 /**
  *
@@ -13,13 +17,18 @@ import java.util.Scanner;
  */
 public class TokenisingString {
     public static void main(String[] Args){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a sentence and press Enter");
-        String sentence = scanner.nextLine();
-        String[] tokens = sentence.split(" ");
-        System.out.printf("Number of elements: %d%nThe tokens are:%n",tokens.length);
-        for (String token : tokens)
-        System.out.println(token);
+        String fileName = "C:\\Users\\Win-7\\Desktop\\HotelReservation.txt";
+        try (Stream<String> stream = Files.lines(Paths.get(fileName))){
+            stream.forEach(System.out::println);
+        } catch (Exception e) {
+        }
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter a sentence and press Enter");
+//        String sentence = scanner.nextLine();
+//        String[] tokens = sentence.split(" ");
+//        System.out.printf("Number of elements: %d%nThe tokens are:%n",tokens.length);
+//        for (String token : tokens)
+//        System.out.println(token);
     }
     
 }
